685a8cb8dfeba9557da20fdaff907d47a5d00476
k <31718966+requiesce@users.noreply.github.com>
2020-12-07 19:43:00 -0800

    created start scene, moved network manager to start

---
+++ Assets/Scripts/RemoveNetworkHUD.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class RemoveNetworkHUD : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject netMgr = GameObject.Find("networkManager");
+        NetworkManagerHUD netMgrHUD = netMgr.GetComponent<NetworkManagerHUD>();
+        netMgrHUD.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
